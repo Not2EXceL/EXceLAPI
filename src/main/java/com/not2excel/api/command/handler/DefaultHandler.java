7066ddedb9939777d3363e8a8c17d5fe4e225f1f
Richmond Steele <not2excel@gmail.com>
2013-12-24 20:44:44 -1000

    First commit. Added CommandAPI

---
+++ src/main/java/com/not2excel/api/command/handler/DefaultHandler.java
@@ -0,0 +1,118 @@
+package com.not2excel.api.command.handler;
+
+import com.not2excel.api.command.objects.ChildCommand;
+import com.not2excel.api.command.objects.CommandInfo;
+import com.not2excel.api.command.objects.Parent;
+import com.not2excel.api.command.objects.QueuedCommand;
+import com.not2excel.api.util.Colorizer;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+/**
+ * @author Richmond Steele
+ * @since 12/18/13
+ * All rights Reserved
+ * Please read included LICENSE file
+ */
+public class DefaultHandler implements Handler
+{
+    private final QueuedCommand queue;
+
+    public DefaultHandler(QueuedCommand queue)
+    {
+        this.queue = queue;
+    }
+
+    @Override
+    public void handleCommand(CommandInfo info) throws CommandException
+    {
+        List<String> strings = info.getArgs();
+        Parent parent = info.getParent();
+        String command = info.getCommand();
+
+        if (strings.size() == 0 || parent.getChildCommands().size() == 0)
+        {
+            if (queue != null)
+            {
+                if (info.getArgsLength() < info.getCommandHandler().min())
+                {
+                    info.getSender().sendMessage("Too few arguments.");
+                    info.getRegisteredCommand().displayDefaultUsage(info.getSender(), command, info.getParent());
+                    throw new CommandException("Too few arguments.");
+                }
+                if (info.getCommandHandler().max() != -1 && info.getArgsLength() > info.getCommandHandler().max())
+                {
+                    info.getSender().sendMessage("Too many arguments.");
+                    info.getRegisteredCommand().displayDefaultUsage(info.getSender(), command, info.getParent());
+                    throw new CommandException("Too many arguments.");
+                }
+                info.setArgs(info.getRegisteredCommand().sortQuotedArgs(info.getArgs()));
+                try
+                {
+                    queue.getMethod().invoke(queue.getObject(), info);
+                }
+                catch (IllegalAccessException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (InvocationTargetException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                info.getRegisteredCommand().displayDefaultUsage(info.getSender(), command, info.getParent());
+            }
+        }
+        if (strings.size() > 0)
+        {
+            if (strings.get(0).equalsIgnoreCase("help") && !parent.getChildCommands().containsKey("help"))
+            {
+                if (info.getUsage().equals(""))
+                {
+                    info.getRegisteredCommand().displayDefaultUsage(info.getSender(), command, info.getParent());
+                }
+                else
+                {
+                    info.getSender().sendMessage(info.getUsage());
+                }
+                return;
+            }
+            synchronized (parent.getChildCommands())
+            {
+                ChildCommand child = parent.getChildCommands().get(strings.get(0));
+                if (child == null)
+                {
+                    if (info.getUsage().equals(""))
+                    {
+                        info.getRegisteredCommand().displayDefaultUsage(info.getSender(), command, info.getParent());
+                    }
+                    else
+                    {
+                        info.getSender().sendMessage(info.getUsage());
+                    }
+                    return;
+                }
+                if (!child.checkPermission(info.getSender()))
+                {
+                    Colorizer.send(info.getSender(), "<red>" + child.getCommandHandler().noPermission());
+                }
+                CommandInfo cmdInfo = new CommandInfo(info.getRegisteredCommand(), child, child.getCommandHandler(),
+                                                      info.getSender(), strings.get(0),
+                                                      strings.subList(1, strings.size() - 1),
+                                                      info.getUsage(),
+                                                      info.getPermission());
+                try
+                {
+                    child.getHandler().handleCommand(cmdInfo);
+                }
+                catch (CommandException e)
+                {
+                    Colorizer.send(info.getSender(), "<red>Failed to handle command properly.");
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/not2excel/api/command/objects/CommandInfo.java
@@ -0,0 +1,219 @@
+package com.not2excel.api.command.objects;
+
+import com.not2excel.api.command.CommandHandler;
+import com.not2excel.api.command.handler.CommandException;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+/**
+ * @author Richmond Steele
+ * @since 12/17/13
+ * All rights Reserved
+ * Please read included LICENSE file
+ */
+public class CommandInfo
+{
+    private final RegisteredCommand registeredCommand;
+    private final Parent            parent;
+    private final CommandHandler    commandHandler;
+    private final CommandSender     sender;
+    private final String            command;
+    private final String            usage;
+    private final String            permission;
+    private       List<String>          args;
+
+    public CommandInfo(RegisteredCommand registeredCommand, Parent parent, CommandHandler commandHandler,
+                       CommandSender sender, String command, List<String> args, String usage, String permission)
+    {
+        this.registeredCommand = registeredCommand;
+        this.parent = parent;
+        this.commandHandler = commandHandler;
+        this.sender = sender;
+        this.command = command;
+        this.args = args;
+        this.usage = usage;
+        this.permission = permission;
+    }
+
+    public RegisteredCommand getRegisteredCommand()
+    {
+        return registeredCommand;
+    }
+
+    public Parent getParent()
+    {
+        return parent;
+    }
+
+    public CommandHandler getCommandHandler()
+    {
+        return commandHandler;
+    }
+
+    public CommandSender getSender()
+    {
+        return sender;
+    }
+
+    public String getCommand()
+    {
+        return command;
+    }
+
+    public List<String> getArgs()
+    {
+        return args;
+    }
+
+    public void setArgs(List<String> args)
+    {
+        this.args = args;
+    }
+
+    public int getArgsLength()
+    {
+        return args.size();
+    }
+
+    public String getPermission()
+    {
+        if (commandHandler == null)
+        {
+            return permission;
+        }
+        return commandHandler.permission();
+    }
+
+    public String noPermission()
+    {
+        if (commandHandler == null)
+        {
+            return "";
+        }
+        return commandHandler.noPermission();
+    }
+
+    public String getUsage()
+    {
+        if (commandHandler == null)
+        {
+            return usage;
+        }
+        return commandHandler.usage();
+    }
+
+    public String getDescription()
+    {
+        if (commandHandler == null)
+        {
+            return "";
+        }
+        return commandHandler.description();
+    }
+
+    public String getIndex(int index) throws CommandException
+    {
+        if(index > args.size())
+        {
+            throw new CommandException(sender, "<red>Invalid index number");
+        }
+        return args.get(index);
+    }
+
+    public String getIndex(int index, String defaultString)
+    {
+        if(index > args.size())
+        {
+            return defaultString;
+        }
+        return args.get(index);
+    }
+
+    public int getInt(int index) throws CommandException
+    {
+        if(index > args.size())
+        {
+            throw new CommandException(sender, "<red>Invalid index number");
+        }
+        int returnValue;
+        try
+        {
+            returnValue = Integer.parseInt(args.get(index));
+        }
+        catch(NumberFormatException e)
+        {
+            throw new CommandException(sender, "<red>Index <gold>%d<red> is not an Integer", index);
+        }
+        return returnValue;
+    }
+
+    public int getInt(int index, int defaultValue)
+    {
+        if(index > args.size())
+        {
+            return defaultValue;
+        }
+        try
+        {
+            return Integer.parseInt(args.get(index));
+        }
+        catch(NumberFormatException e)
+        {
+            return defaultValue;
+        }
+    }
+
+    public double getDouble(int index) throws CommandException
+    {
+        if(index > args.size())
+        {
+            throw new CommandException(sender, "<red>Invalid index number");
+        }
+        double returnValue;
+        try
+        {
+            returnValue = Double.parseDouble(args.get(index));
+        }
+        catch(NumberFormatException e)
+        {
+            throw new CommandException(sender, "<red>Index <gold>%d<red> is not an Double", index);
+        }
+        return returnValue;
+    }
+
+    public double getDouble(int index, double defaultValue)
+    {
+        if(index > args.size())
+        {
+            return defaultValue;
+        }
+        try
+        {
+            return Double.parseDouble(args.get(index));
+        }
+        catch(NumberFormatException e)
+        {
+            return defaultValue;
+        }
+    }
+
+    public String joinArgs(int index) throws CommandException
+    {
+        if(index > args.size())
+        {
+            throw new CommandException(sender, "<red>Invalid index number");
+        }
+        StringBuilder builder = new StringBuilder();
+        for(int i = index; i < args.size(); ++i)
+        {
+            String arg = args.get(i);
+            if(i != index)
+            {
+                builder.append(" ");
+            }
+            builder.append(arg);
+        }
+        return builder.toString();
+    }
+}
